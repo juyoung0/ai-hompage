9c626d2c7744250bed1bec056500dc9d37593b7a
juyoung0 <dhwnahd625>
2017-09-29 21:41:23 +0900

    second commit

---
+++ home/urls.py
@@ -0,0 +1,3 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from home import *
 
 # Create your views here.
+def index(request):
+        return render(request, 'index.html')
